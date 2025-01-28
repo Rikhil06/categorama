0652bbc36914a4bc430c7c50e40c506c9c23d0a1
Rikhil Makwana <rikhil@Rikhils-MacBook-Pro.local>
2025-01-28 14:26:55 +0000

    Add GTM and GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), Sitemap({hostname: 'https://categorama.netlify.app/'})],
+  plugins: [
+    react(), 
+    Sitemap({hostname: 'https://categorama.netlify.app/'}),
+    VitePluginRadar({ analytics: { id: 'G-4QM1632KQ0', }}),
+  ],
   build: {
     target: "es2022"
   },
