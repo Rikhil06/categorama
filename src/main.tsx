a4af14dcfed79bab70919431df0485170a5254f6
Rikhil Makwana <rikhil@Mac.communityfibre.co.uk>
2025-09-08 16:01:27 +0100

    Add Router for connect page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Connect from './connect.tsx'
 import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/connect" element={<Connect />} />
+        </Routes>
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
 )
